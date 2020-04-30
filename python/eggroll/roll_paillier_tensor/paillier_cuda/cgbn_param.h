8d5072d07aab8903d3e193533175d13c6f5503a8
RenZH <zh2016@mail.ustc.edu.cn>
2020-04-30 15:57:04 +0800

    delete unused header

--- python/eggroll/roll_paillier_tensor/paillier_cuda/cgbn_param.h
+++
@@ -1,5 +0,0 @@
-#ifndef PAILLIER_CUDA_CGBN_PARAM
-#define PAILLIER_CUDA_CGBN_PARAM
-#include "cgbn/cgbn.h"
-
-#endif
--- python/eggroll/roll_paillier_tensor/paillier_cuda/fixedpoint.h
+++
@@ -1,6 +0,0 @@
-#ifndef FIXEDPOINTNUMEBR_H_INCLUDED
-#define FIXEDPOINTNUMEBR_H_INCLUDED
-#include <stdint.h>
-#include "cgbn/cgbn.h"
-// #define PLAIN_INT64
-#endif
